179800453baf54560d36d657547cc69c2c393bb2
magdybadr978 <magdibadr2002@gmail.com>
2024-06-14 00:53:30 +0300

    add env

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-import express from 'express' // Import Express framework
-import db_connection  from './DB/connection.js' // Import database connection module
-import trainerRouter from './src/Modules/Trainers/trainers.routes.js' // Import trainers routes
-import memberRouter from './src/Modules/Members/members.routes.js' // Import members routes
-import { globalErrorHandling } from './src/utils/ErrorHandling.js'
-
+import express from "express"; // Import Express framework
+import db_connection from "./DB/connection.js"; // Import database connection module
+import trainerRouter from "./src/Modules/Trainers/trainers.routes.js"; // Import trainers routes
+import memberRouter from "./src/Modules/Members/members.routes.js"; // Import members routes
+import { globalErrorHandling } from "./src/utils/ErrorHandling.js";
+import dotenv from "dotenv";
+// set directory name
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "./config/.env") });
 // Initialize the Express application
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-
-db_connection
+db_connection;
 
 // Define routes
-app.use('/trainer', trainerRouter)
-app.use('/member', memberRouter)
+app.use("/trainer", trainerRouter);
+app.use("/member", memberRouter);
 
-app.all("*" , (req,res,next)=>{
-  return res.send("Invalid Routing")
-})
+app.all("*", (req, res, next) => {
+  return res.send("Invalid Routing");
+});
 
-app.use(globalErrorHandling)
+app.use(globalErrorHandling);
 
 // Start the server and listen on the specified port
-app.listen(port, ()=>console.log(`Server Is Running on port ${port}`))
+app.listen(port, () => console.log(`Server Is Running on port ${port}`));
